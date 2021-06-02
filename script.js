21a4b7687c39ec8364b50ef5f568c634cc5bd437
Giedrius <paurys79@gmail.com>
2021-06-02 14:49:14 +0300

    js namu darbas

---
+++ script.js
@@ -0,0 +1,46 @@
+// Pirma namų darbų Užduotis
+
+prompt("Kokia prekės kaina?");
+
+// Prekės kaina su PVM
+
+// var price = 23.07;
+// var PVM = 4.8447;
+// var priceNoPVM = 18.2253;
+// var sum = priceNoPVM + PVM;
+// console.log(sum.toFixed(2));
+
+// PVM
+
+// var price = 23.07;
+// var PVM = 4.8447;
+// var priceNoPVM = 18.2253;
+// var minus = price - priceNoPVM;
+// console.log(minus.toFixed(2));
+
+// Kaina bbe PVM
+
+// var price = 23.07;
+// var PVM = 4.8447;
+// var priceNoPVM = 18.2253;
+// var minus = price - PVM;
+// console.log(minus.toFixed(2));
+// console.log("23.07 + 18.2253 + €");
+
+
+
+// Antra namų darbų Užduotis
+
+
+// var name = prompt("Koks tavo vardas?");
+// alert("Naujas dalyvis: " + name);
+
+// var names = ["JonSnow", "Cersei Lannister", "Daenerys Targaryen",
+// "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+// console.log(names[0] + ", " + names[5] + ". ");
+
+
+// var names = ["JonSnow", "Cersei Lannister", "Daenerys Targaryen",
+// "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+// var number = prompt("Koks tai skaičius?");
+// console.log("Dalyvio vardas: " + names[number] + ". ");
